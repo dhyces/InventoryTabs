43f6bb34e59af4e1578b1a8bab782d06f67b2779
dhyces <10985914+dhyces@users.noreply.github.com>
2022-12-11 12:54:36 -0500

    Update gradle and fix configs

--- src/main/java/com/kqp/inventorytabs/init/InventoryTabsConfig.java
+++ src/main/java/com/kqp/inventorytabs/init/InventoryTabsConfig.java
@@ -2,14 +2,15 @@ package com.kqp.inventorytabs.init;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InventoryTabsConfig {
 
     public static ForgeConfigSpec.BooleanValue doSightChecksFlag;
     public static ForgeConfigSpec.BooleanValue rotatePlayer;
-    public static ForgeConfigSpec.ConfigValue<List<String>> excludeTab;
-    public static ForgeConfigSpec.ConfigValue<List<String>> includeTab;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> excludeTab;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> includeTab;
     public static ForgeConfigSpec.BooleanValue renderTabs;
     public static ForgeConfigSpec.BooleanValue debugEnabled;
 
@@ -18,8 +19,8 @@ public class InventoryTabsConfig {
 
         doSightChecksFlag = builder.define("doSightChecksFlag", () -> true);
         rotatePlayer = builder.define("rotatePlayer", () -> false);
-        excludeTab = builder.define("excludeTab", List.of("tiered:reforging_station", "#techreborn:block_entities_without_inventories", "#inventorytabs:mod_compat_blacklist"));
-        includeTab = builder.define("includeTab", List.of());
+        excludeTab = builder.defineList("excludeTab", Arrays.asList("tiered:reforging_station", "#techreborn:block_entities_without_inventories", "#inventorytabs:mod_compat_blacklist"), o -> true);
+        includeTab = builder.defineList("includeTab", Arrays.asList(""), o -> true);
         renderTabs = builder.define("renderTabs", () -> true);
         debugEnabled = builder.define("debugEnabled", () -> false);
     }
